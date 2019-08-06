bb079a1f0cb9fa3041a362e3a638253aa416b733
Alex <workpost2017@i.ua>
2019-08-06 21:51:32 +0300

    angular start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,50 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { AboutComponent } from './about/about.component';
+import { ClassesComponent } from './classes/classes.component';
+import { ContactComponent } from './contact/contact.component';
+import { ElementsComponent } from './elements/elements.component';
+import { BlogComponent } from './blog/blog.component';
+
+import { RouterModule, Routes } from '@angular/router';
+
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+//ng generate module app-routing --flat --module=app
+//або так:
+const appRoutes: Routes = [
+  { path: '', component: MainComponent},
+  { path: 'blog', component: BlogComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'classes', component: ClassesComponent},
+  { path: 'contact', component: ContactComponent},
+  { path: 'elements', component: ElementsComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    AboutComponent,
+    ClassesComponent,
+    ContactComponent,
+    ElementsComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true}
+    ),
+    ReactiveFormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
---
+++ src/app/main/main.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+//ng g component main
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
